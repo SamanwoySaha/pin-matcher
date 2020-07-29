c505650f0139c6dedd23dca38dd5832dbc4bb27f
SamanwoySaha <samanwoy.kuet@gmail.com>
2020-07-29 13:04:04 +0600

    added pin generator and error-handler

---
+++ script.js
@@ -0,0 +1,45 @@
+// pin generator event handler
+let generateBtn = document.getElementById('generate-btn');
+generateBtn.addEventListener('click', function() {
+    let pinNumber = pinGenerator();
+    document.getElementById('pinNumber').value = pinNumber;
+});
+
+function pinGenerator(){
+    let pinNumber = Math.round(Math.random() * 9999);
+
+    if(pinNumber >= 1000 && pinNumber <= 9999) {
+        return pinNumber;
+    }
+    else {
+        return pinGenerator();
+    }
+}
+
+// number button event handler
+let number = document.getElementsByClassName('number');
+for(var i = 0; i < number.length - 1; i++) {
+    let userInput = document.getElementById('user-input');
+    number[i].addEventListener('click', function() {
+        console.log(number[i]);
+        userInput.value = userInput.value + this.id; 
+    });
+}
+
+// submit button event handler
+let submitBtn = document.getElementById('submit');
+submitBtn.addEventListener('click', function() {
+    let pinNumber = document.getElementById('pinNumber').value;
+    let userInput = document.getElementById('user-input').value;
+    console.log(pinNumber, userInput);
+    if(pinNumber == userInput) {
+        document.getElementById('verify-message').style.display = 'block';
+        document.getElementById('error-message').style.display = 'none';
+        document.getElementById('user-input').value = '';
+    }
+    else {
+        document.getElementById('verify-message').style.display = 'none';
+        document.getElementById('error-message').style.display = 'block';
+        document.getElementById('user-input').value = '';
+    }
+});
